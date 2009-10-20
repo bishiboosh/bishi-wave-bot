05f20c9c3f9a6894fe0b861b01af3c197cf24714
bishiboosh <bishiboosh@afab784a-bcf9-11de-869a-473eb4776397>
2009-10-20 15:19:44 +0000

    ca marche paaaaas mais j'ai pas le temps en journée

--- Bishi's bot/src/eu/sweetlygeek/parsers/DropularGetter.java
+++ Bishi's bot/src/eu/sweetlygeek/parsers/DropularGetter.java
@@ -1,6 +1,5 @@
 package eu.sweetlygeek.parsers;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -8,22 +7,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import com.google.appengine.api.urlfetch.HTTPResponse;
 import com.google.appengine.api.urlfetch.URLFetchService;
 import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
 import com.google.wave.api.Blip;
-import com.google.wave.api.Image;
 import com.google.wave.api.Wavelet;
 
 /** Dropular blip parser
@@ -110,35 +103,6 @@ public class DropularGetter extends BlipParser {
 	}
 
 	private void parseResponse(byte[] content, Wavelet wavelet) throws ParserConfigurationException, SAXException, IOException {
-//		List<Image> images = new ArrayList<Image>();
-//		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-//		DocumentBuilder db = dbf.newDocumentBuilder();
-//		Document d = db.parse(new ByteArrayInputStream(content));
-//
-//		Element root = d.getDocumentElement();
-//		NodeList drops = root.getElementsByTagName("drop");
-//
-//		Blip b = wavelet.appendBlip();
-//		TextView tv = b.getDocument();
-//		for (Element drop : drops)
-//		{
-//			String imgUrl = drop.elementText("image_big");
-//			if (imgUrl != null && !"".equals(imgUrl))
-//			{
-//				Image i = new Image();
-//				i.setUrl(imgUrl);
-//				images.add(i);
-//				tv.append(imgUrl);
-//			}
-//		}
-//		if (!images.isEmpty())
-//		{
-//			Blip b = wavelet.appendBlip();
-//			TextView view = b.getDocument();
-//			for (Image i : images)
-//			{
-//				view.appendElement(i);
-//			}
-//		}
+		// TODO
 	}
 }
