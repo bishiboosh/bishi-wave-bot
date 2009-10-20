c7e7ba3dc435470b8e15b749d7f7e5adbabab564
bishiboosh <bishiboosh@afab784a-bcf9-11de-869a-473eb4776397>
2009-10-20 11:01:33 +0000

    Rajout dropular + log

--- Bishi's bot/src/eu/sweetlygeek/DropularGetter.java
+++ Bishi's bot/src/eu/sweetlygeek/DropularGetter.java
@@ -1,37 +1,53 @@
 package eu.sweetlygeek;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.log4j.Logger;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 
 import com.google.wave.api.Blip;
+import com.google.wave.api.Image;
+import com.google.wave.api.TextView;
 import com.google.wave.api.Wavelet;
 
 /** Dropular blip parser
  * @author bishiboosh
  *
  */
-public class DropularGetter implements BlipParser {
+public class DropularGetter extends BlipParser {
 	
-	private static final String DROPTACULAR_TAG = "droptacular";
+	private static final Logger LOGGER = Logger.getLogger(DropularGetter.class);
+	
+	public static final String DROPULAR_TAG = "dropular:";
 
 	public static final String DROPULAR_URL = "http://dropular.net/api/";
-	
+
 	private static DropularGetter instance;
 	private HttpClient client;
-	
-	private DropularGetter()
+	private SAXReader reader;
+
+	private DropularGetter() throws ParserConfigurationException
 	{
 		this.client = new DefaultHttpClient();
+		this.reader = new SAXReader();
 	}
-	
-	public static DropularGetter getInstance()
+
+	public static DropularGetter getInstance() throws ParserConfigurationException
 	{
 		if (instance == null)
 		{
@@ -48,7 +64,7 @@ public class DropularGetter implements BlipParser {
 		boolean found = false;
 		for (String word : words)
 		{
-			if (StringUtils.contains(word, DROPTACULAR_TAG))
+			if (StringUtils.contains(word, DROPULAR_TAG))
 			{
 				found = true;
 				requests.add(word);
@@ -62,19 +78,65 @@ public class DropularGetter implements BlipParser {
 		{
 			for (String request : requests)
 			{
-				analyzeRequest(request);
+				analyzeRequest(request, currentWavelet);
 			}
 		}
 	}
-	
-	private void analyzeRequest(String request)
+
+	private void analyzeRequest(String request, Wavelet currentWavelet)
 	{
-		String[] params = request.split(":");
-		if (params.length == 2)
+		try {
+			String[] params = request.split(":");
+			if (params.length == 2)
+			{
+				// Par default : pool
+				HttpGet get = new HttpGet(DROPULAR_URL + "get_pool/" + params[1] + "/random/10.xml");
+				HttpResponse res = client.execute(get);
+				int status = res.getStatusLine().getStatusCode();
+				if (status == 200)
+				{
+					HttpEntity entity = res.getEntity();
+					if (entity != null)
+					{
+						parseResponse(entity, currentWavelet);
+					}
+				}
+			}
+		} catch (ClientProtocolException e) {
+			LOGGER.error("Error while analyzing request", e);
+		} catch (IllegalStateException e) {
+			LOGGER.error("Error while analyzing request", e);
+		} catch (IOException e) {
+			LOGGER.error("Error while analyzing request", e);
+		} catch (DocumentException e) {
+			LOGGER.error("Error while analyzing request", e);
+		}
+		// TODO : autre que keyword
+	}
+
+	private void parseResponse(HttpEntity entity, Wavelet wavelet) throws IllegalStateException, DocumentException, IOException {
+		List<Image> images = new ArrayList<Image>();
+		Document doc = this.reader.read(entity.getContent());
+		Element root = doc.getRootElement();
+		List<Element> drops = root.elements("drop");
+		for (Element drop : drops)
 		{
-			// Par défault : keyword
-			HttpGet g = new HttpGet(DROPULAR_URL + "get_keyword/" + params[1] + "/latest/10.xml");
-			//HttpResponse r = client.execute(g);
+			String imgUrl = drop.elementText("image_big");
+			if (imgUrl != null && !"".equals(imgUrl))
+			{
+				Image i = new Image();
+				i.setUrl(imgUrl);
+				images.add(i);
+			}
+		}
+		if (!images.isEmpty())
+		{
+			Blip b = wavelet.appendBlip();
+			TextView view = b.getDocument();
+			for (Image i : images)
+			{
+				view.appendElement(i);
+			}
 		}
 	}
 }
