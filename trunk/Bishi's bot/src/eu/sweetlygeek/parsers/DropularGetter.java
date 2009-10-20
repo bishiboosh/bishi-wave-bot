e17b3366f57115c45b77d4d6c90b0b8c6c54bc54
bishiboosh <bishiboosh@afab784a-bcf9-11de-869a-473eb4776397>
2009-10-20 12:28:46 +0000

    ca marche paaaaas mais j'ai pas le temps en journée

--- trunk/Bishi's bot/src/eu/sweetlygeek/parsers/DropularGetter.java
+++ trunk/Bishi's bot/src/eu/sweetlygeek/parsers/DropularGetter.java
@@ -1,51 +1,49 @@
 package eu.sweetlygeek.parsers;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.log4j.Logger;
-import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.Element;
-import org.dom4j.io.SAXReader;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import com.google.appengine.api.urlfetch.HTTPResponse;
+import com.google.appengine.api.urlfetch.URLFetchService;
+import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
 import com.google.wave.api.Blip;
 import com.google.wave.api.Image;
-import com.google.wave.api.TextView;
 import com.google.wave.api.Wavelet;
-
-import eu.sweetlygeek.http.NoSSLHttpClient;
 
 /** Dropular blip parser
  * @author bishiboosh
  *
  */
 public class DropularGetter extends BlipParser {
-	
+
 	private static final Logger LOGGER = Logger.getLogger(DropularGetter.class);
-	
+
 	public static final String DROPULAR_TAG = "dropular:";
 
 	public static final String DROPULAR_URL = "http://dropular.net/api/";
 
 	private static DropularGetter instance;
-	private HttpClient client;
-	private SAXReader reader;
+	private URLFetchService fetcher;
 
 	private DropularGetter() throws ParserConfigurationException
 	{
-		this.client = new NoSSLHttpClient();
-		this.reader = new SAXReader();
+		this.fetcher = URLFetchServiceFactory.getURLFetchService();
 	}
 
 	public static DropularGetter getInstance() throws ParserConfigurationException
@@ -91,53 +89,56 @@ public class DropularGetter extends BlipParser {
 			if (params.length == 2)
 			{
 				// Par default : pool
-				HttpGet get = new HttpGet(DROPULAR_URL + "get_pool/" + params[1] + "/random/10.xml");
-				HttpResponse res = client.execute(get);
-				int status = res.getStatusLine().getStatusCode();
-				if (status == 200)
+				URL url = new URL(DROPULAR_URL + "get_pool/" + params[1] + "/random/10.xml");
+				HTTPResponse res = this.fetcher.fetch(url);
+				if (res != null)
 				{
-					HttpEntity entity = res.getEntity();
-					if (entity != null)
-					{
-						parseResponse(entity, currentWavelet);
-					}
+					parseResponse(res.getContent(), currentWavelet);
 				}
 			}
-		} catch (ClientProtocolException e) {
-			LOGGER.error("Error while analyzing request", e);
-		} catch (IllegalStateException e) {
+		} catch (MalformedURLException e) {
 			LOGGER.error("Error while analyzing request", e);
 		} catch (IOException e) {
 			LOGGER.error("Error while analyzing request", e);
-		} catch (DocumentException e) {
+		}
+		catch (ParserConfigurationException e) {
+			LOGGER.error("Error while analyzing request", e);
+		} catch (SAXException e) {
 			LOGGER.error("Error while analyzing request", e);
 		}
 		// TODO : autre que keyword
 	}
 
-	private void parseResponse(HttpEntity entity, Wavelet wavelet) throws IllegalStateException, DocumentException, IOException {
-		List<Image> images = new ArrayList<Image>();
-		Document doc = this.reader.read(entity.getContent());
-		Element root = doc.getRootElement();
-		List<Element> drops = root.elements("drop");
-		for (Element drop : drops)
-		{
-			String imgUrl = drop.elementText("image_big");
-			if (imgUrl != null && !"".equals(imgUrl))
-			{
-				Image i = new Image();
-				i.setUrl(imgUrl);
-				images.add(i);
-			}
-		}
-		if (!images.isEmpty())
-		{
-			Blip b = wavelet.appendBlip();
-			TextView view = b.getDocument();
-			for (Image i : images)
-			{
-				view.appendElement(i);
-			}
-		}
+	private void parseResponse(byte[] content, Wavelet wavelet) throws ParserConfigurationException, SAXException, IOException {
+//		List<Image> images = new ArrayList<Image>();
+//		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+//		DocumentBuilder db = dbf.newDocumentBuilder();
+//		Document d = db.parse(new ByteArrayInputStream(content));
+//
+//		Element root = d.getDocumentElement();
+//		NodeList drops = root.getElementsByTagName("drop");
+//
+//		Blip b = wavelet.appendBlip();
+//		TextView tv = b.getDocument();
+//		for (Element drop : drops)
+//		{
+//			String imgUrl = drop.elementText("image_big");
+//			if (imgUrl != null && !"".equals(imgUrl))
+//			{
+//				Image i = new Image();
+//				i.setUrl(imgUrl);
+//				images.add(i);
+//				tv.append(imgUrl);
+//			}
+//		}
+//		if (!images.isEmpty())
+//		{
+//			Blip b = wavelet.appendBlip();
+//			TextView view = b.getDocument();
+//			for (Image i : images)
+//			{
+//				view.appendElement(i);
+//			}
+//		}
 	}
 }
