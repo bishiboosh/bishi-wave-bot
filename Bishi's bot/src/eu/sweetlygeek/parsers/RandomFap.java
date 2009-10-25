c2fc09d98fb126ef0b1864f086a2911fd6023f0b
bishiboosh <bishiboosh@afab784a-bcf9-11de-869a-473eb4776397>
2009-10-25 22:24:12 +0000

    Rajout du stockage en base

--- Bishi's bot/src/eu/sweetlygeek/parsers/RandomFap.java
+++ Bishi's bot/src/eu/sweetlygeek/parsers/RandomFap.java
@@ -1,27 +1,64 @@
 package eu.sweetlygeek.parsers;
 
+import java.util.List;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+
+import org.apache.commons.lang.RandomStringUtils;
+
 import com.google.wave.api.Blip;
 import com.google.wave.api.Gadget;
 import com.google.wave.api.Wavelet;
 
+import eu.sweetlygeek.persistent.GeneratedResponse;
+import eu.sweetlygeek.servlets.RandomFapServlet;
+import eu.sweetlygeek.utils.Utils;
+
 public class RandomFap extends BlipParser {
-	
+
 	private static final String FAP_TAG = "bot:fap";
-	
-	private static final String GADGET_URL = "http://bishibot.appspot.com/_wave/robot/fap.xml";
-	
+
+	private static final String GADGET_URL = "http://bishibot.appspot.com/_wave/robot/fap/";
+	private static final String URL_NAME = "/_wave/robot/fap/";
+
+	private Query queryUrl;
+
+	public RandomFap()
+	{
+		PersistenceManager pm = Utils.getInstance().getPersistenceManager();
+		queryUrl = pm.newQuery(RandomFapServlet.QUERY_URL);
+	}
+
 	@Override
 	protected void analyzeRequest(String request, Wavelet currentWavelet) {
 		Blip blip = currentWavelet.appendBlip();
-		
-		Gadget g = new Gadget(GADGET_URL);
-		
+
+		String xmlName = RandomStringUtils.randomAlphanumeric(10);
+
+		while (urlExists(xmlName))
+		{
+			xmlName = RandomStringUtils.randomAlphanumeric(10);
+		}
+
+		String url = GADGET_URL + xmlName + ".xml";
+
+		Gadget g = new Gadget(url);
+
 		blip.getDocument().appendElement(g);
 	}
 
 	@Override
 	protected String getTag() {
 		return FAP_TAG;
+	}
+
+	@SuppressWarnings("unchecked")
+	private boolean urlExists(String xmlName)
+	{
+		String url = URL_NAME + xmlName + ".xml";
+		List<GeneratedResponse> urls = (List<GeneratedResponse>) queryUrl.execute(url);
+		return !urls.isEmpty();
 	}
 
 }
